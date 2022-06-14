83024a947dfefba6b2a3fff7d317a5328c3635c8
Hakob Tumanyan <jamestumanyan@Jamess-MacBook-Air.local>
2022-06-15 00:00:03 +0400

    going on

--- srcs/maria/init.sh
+++ srcs/maria/init.sh
@@ -1,6 +1,6 @@
 source .env
-mysql -e "CREATE DATABASE $db_name;"
-mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
-mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'$hostname' WITH GRANT OPTION;"
-mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
-mysql -e "FLUSH PRIVILEGES;"
+mariadb -e "CREATE DATABASE $db_name;"
+mariadb -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
+mariadb -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'$hostname' WITH GRANT OPTION;"
+mariadb -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
+mariadb -e "FLUSH PRIVILEGES;"
