5e40bb523404cad79accca44240a681c14906606
Hakob Tumanyan <htumanya@c2r5s6.42yerevan.am>
2022-06-25 21:15:12 +0400

    message

---
+++ srcs/wp/conf/wp-config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define( 'DB_NAME', 'wp_base' );
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASSWORD', 'wp_secret_12345' );
+    define( 'DB_HOST', 'localhost' );
+    define( 'DB_CHARSET', 'utf8' );
+
+?>
