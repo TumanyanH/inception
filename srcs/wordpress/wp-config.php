466706cbe0d5eee35263f5d149ab3b0f8ccbbdca
Hakob Tumanyan <jamestumanyan@Jamess-MacBook-Air.local>
2022-06-27 00:28:57 +0400

    message

---
+++ srcs/wordpress/wp-config.php
@@ -0,0 +1,19 @@
+<?php
+
+    define( 'DB_NAME', 'wp_base' );
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASSWORD', 'wp_secret_12345' );
+    define( 'DB_HOST', 'localhost' );
+    define( 'DB_CHARSET', 'utf8' );
+
+
+    $table_prefix = 'wp_';
+
+    define( 'WP_DEBUG', false );
+
+    if ( ! defined( 'ABSPATH' ) ) {
+    	define( 'ABSPATH', '/var/www/html/wordpress' );
+    }
+
+    require_once ABSPATH . 'wp-settings.php';
+?>
