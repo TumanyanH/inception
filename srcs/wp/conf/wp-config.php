990daa121282d64b11086fb4f176dfefccbc1953
Hakob Tumanyan <htumanya@c2r5s6.42yerevan.am>
2022-06-27 17:47:12 +0400

    message

--- srcs/wp/conf/wp-config.php
+++ srcs/wp/conf/wp-config.php
@@ -3,7 +3,7 @@
     define( 'DB_NAME', 'wp_base' );
     define( 'DB_USER', 'root' );
     define( 'DB_PASSWORD', 'wp_secret_12345' );
-    define( 'DB_HOST', 'localhost' );
+    define( 'DB_HOST', 'mariadb:3306' );
     define( 'DB_CHARSET', 'utf8' );
 
 
